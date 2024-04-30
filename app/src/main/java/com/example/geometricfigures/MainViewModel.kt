61ccbd8d4cf455fc45d0369022db25fd4dc18a27
Enriquermg21 <enriquermg21@gmail.com>
2024-04-30 19:21:03 +0200

    Strings and continue with second view, resolve items

--- app/src/main/java/com/example/geometricfigures/MainViewModel.kt
+++ app/src/main/java/com/example/geometricfigures/MainViewModel.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import androidx.lifecycle.ViewModel
 
+
 class MainViewModel : ViewModel() {
     fun swapview(context: Context) {
         val intent = Intent(context, AreaActivity::class.java)
